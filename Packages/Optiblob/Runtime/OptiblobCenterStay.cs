2a0bf9c9fd57d4b48b5206c3bb0f294ad6ef96e9
Edwon <edwonia@gmail.com>
2020-12-21 20:10:56 +0900

    added UpdateOption to OptiblobCenterStay component

--- Packages/Optiblob/Runtime/OptiblobCenterStay.cs
+++ Packages/Optiblob/Runtime/OptiblobCenterStay.cs
@@ -7,18 +7,25 @@ namespace Optiblob
     public class OptiblobCenterStay : MonoBehaviour
     {
         public Optiblob optiblob;
-        public bool useFixedUpdate = false;
+        public enum UpdateOption {Update, FixedUpdate, LateUpdate}
+        public UpdateOption updateOption;
 
         void Update()
         {
-            if (!useFixedUpdate)
+            if (updateOption == UpdateOption.Update)
                 transform.position = optiblob.BlobCenter;
         }
 
 
         void FixedUpdate()
         {
-            if (useFixedUpdate)
+            if (updateOption == UpdateOption.FixedUpdate)
+                transform.position = optiblob.BlobCenter;
+        }
+
+        void LateUpdate()
+        {
+            if (updateOption == UpdateOption.LateUpdate)
                 transform.position = optiblob.BlobCenter;
         }
     }
