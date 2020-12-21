799ad463b6e07a1c65d4fce34c8dea74334f6cb3
Edwon <edwonia@gmail.com>
2020-12-21 20:03:01 +0900

    added OptiblobCenterStay component

---
+++ Packages/Optiblob/Runtime/OptiblobCenterStay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Optiblob
+{
+    public class OptiblobCenterStay : MonoBehaviour
+    {
+        public Optiblob optiblob;
+        public bool useFixedUpdate = false;
+
+        void Update()
+        {
+            if (!useFixedUpdate)
+                transform.position = optiblob.BlobCenter;
+        }
+
+
+        void FixedUpdate()
+        {
+            if (useFixedUpdate)
+                transform.position = optiblob.BlobCenter;
+        }
+    }
+}
